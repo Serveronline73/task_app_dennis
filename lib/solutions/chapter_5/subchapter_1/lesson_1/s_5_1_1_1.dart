85c4c7ba030c55bd5ff5ac4679def6e191e34098
dennis <info.dennis.durmus@gmail.com>
2024-10-11 13:54:19 +0200

    Afgabe 5.1.1.1 erledigt

--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_1.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_1.dart
@@ -4,6 +4,44 @@ class S5111 extends StatelessWidget {
   const S5111({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Hello World !",
+            style: TextStyle(fontSize: 20),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                margin: const EdgeInsets.all(8.0),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                margin: const EdgeInsets.all(8.0),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+          Switch(
+            value: true,
+            onChanged: (bool value) {},
+            activeColor: Colors.orange,
+          )
+        ],
+      ),
+    );
   }
 }
